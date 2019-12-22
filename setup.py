1886ce770eadc2a4d8d2cb56197da9ba3192a4bd
npeleg <47854111+npeleg@users.noreply.github.com>
2019-12-22 17:18:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
-# my project's settings.
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'asdf',
+    version = '0.1.0',
+    author = 'Peleg Neufeld',
+    description = 'An example package.',
+    packages = find_packages(),
+    install_requires = ['click', 'flask'],
+    tests_require = ['pytest', 'pytest-cov'],
+)
